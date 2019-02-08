0da76b8067122099e3b5546f771d1bfd9c2739ec
gayathrisakthivel <47449916+gayathrisakthivel@users.noreply.github.com>
2019-02-08 21:07:31 +0530

    Update and rename README.md to positive.c

---
+++ positive.c
@@ -0,0 +1,22 @@
+# C-Programming
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    {
+        printf("the number is zero");
+    }
+  else if(n>0)
+    {
+        printf("the number is positive");
+
+    }
+    else 
+    {
+        printf("the number is negative");
+    }
+    return 0;
+}
+
